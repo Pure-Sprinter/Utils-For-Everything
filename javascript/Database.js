4db4e8de0c17fb8e8769055b68c8f3bb6f36f09c
CEOJINSUNG <jinsung1048@gmail.com>
2022-09-06 17:38:17 +0900

    feat : Database.js 초기 세팅

---
+++ javascript/Database.js
@@ -0,0 +1,56 @@
+import path from "path";
+import sqlite3 from "sqlite3";
+
+const __dirname = path.resolve();
+const sql = sqlite3.verbose();
+const db_path = path.join(__dirname, "/src/sql/data", "payment.db");
+
+export async function get_db() {
+  const db = new sql.Database(db_path, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+
+    console.log("'payment.db'가 성공적으로 생성되었습니다");
+  });
+  return db;
+}
+
+export async function db_run(query, value) {
+  const db = await get_db();
+  console.log(query);
+  return new Promise(function (resolve, reject) {
+    db.run(query, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(value);
+    });
+  });
+}
+
+export async function db_get(query) {
+  const db = await get_db();
+  console.log(query);
+  return new Promise(function (resolve, reject) {
+    db.get(query, (err, row) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(row);
+    });
+  });
+}
+
+export async function db_all(query) {
+  const db = await get_db();
+  console.log(query);
+  return new Promise(function (resolve, reject) {
+    db.all(query, function (err, rows) {
+      if (err) {
+        return reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
