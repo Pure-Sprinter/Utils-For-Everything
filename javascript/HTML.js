de5c4a1c02a8c8701e75d2185c5480e46d1f4ad0
CEOJINSUNG <jinsung1048@gmail.com>
2022-09-06 17:48:31 +0900

    feat : HTML 태그를 접근하는 함수 모음집입니다.

---
+++ javascript/HTML.js
@@ -0,0 +1,45 @@
+/**
+ * 개체 숨기기 함수
+ */
+export function disappear_target(id) {
+  const element = document.getElementById(id);
+  element.style.display = "none";
+  return false;
+}
+
+/**
+ * 개체 색상 및 텍스트 정하는 함수
+ */
+export function set_element_text_and_color({ id, text, color }) {
+  const target = document.getElementById(id);
+  const before_text = target.innerText;
+  target.innerText = text || before_text;
+  target.style.color = color || target.style.color;
+  return { id: id, text: before_text, color: color };
+}
+
+/**
+ * 개체 클릭 이벤트 정하는 함수
+ */
+export function set_element_click_event({ id, listener }) {
+  const target = document.getElementById(id);
+  target.addEventListener("click", listener);
+}
+
+/**
+ * id 를 기반으로 텍스트를 조회
+ */
+export function set_text_by_id({ id, text }) {
+  const target = document.getElementById(id);
+  target.innerText = text;
+  return text;
+}
+
+/**
+ * input value를 정의하는 함수
+ */
+export function set_input_text({ id, text }) {
+  const target = document.getElementById(id);
+  target.value = text;
+  return text;
+}
