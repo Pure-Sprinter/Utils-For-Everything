a99b79790d184c0e5377378e210bdb35f91a057e
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-10 17:13:30 +0900

    refactor : TYPE

--- javascript/Model.js
+++ javascript/Model.js
@@ -1,4 +1,71 @@
 export class Model {
+  constructor() {
+    this.id = 0;
+    this.created_date = "";
+    this.updated_date = "";
+  }
+
+  get(col) {
+    this.not_exist_col_error(col);
+    return this[col];
+  }
+
+  set(col, value) {
+    this.not_exist_col_error(col);
+    this.differ_type_error(col, value);
+    this[col] = value;
+    return this;
+  }
+
+  get_class_name() {
+    return this.constructor.name;
+  }
+
+  columns() {
+    return Object.getOwnPropertyNames(this).filter((col) => col !== "id");
+  }
+
+  types() {
+    return this.columns().map((col) => type_into_sql[typeof this[col]]);
+  }
+
+  values() {
+    return Object.values(this).filter((col) => col !== "id");
+  }
+
+  to_sql() {
+    const types = this.types();
+    return this.columns().map((value, index) => {
+      return { col: value, type: types[index] };
+    });
+  }
+
+  to_entity(object) {
+    const entity = new this.constructor();
+    Object.keys(this).map((key) => {
+      entity[key] = object[key];
+    });
+    return entity;
+  }
+
+  /**
+   * 에러 관련 함수
+   */
+  not_exist_col_error(col) {
+    if (!this.columns().includes(col)) {
+      throw new Error("존재하지 않는 칼럼입니다.");
+    }
+  }
+
+  differ_type_error(col, value) {
+    if (typeof this[col] !== typeof value) {
+      throw new Error("주입한 값의 타입이 다릅니다.");
+    }
+  }
+
+  /**
+   * MySQL 기본 타입
+   */
   static TYPE = {
     /**
      * String Types
@@ -33,70 +100,6 @@ export class Model {
       return "DATETIME";
     },
   };
-
-  constructor() {
-    this.id = 0;
-    this.created_date = "";
-    this.updated_date = "";
-  }
-
-  get(col) {
-    this.not_exist_col_error(col);
-    return this[col];
-  }
-
-  set(col, value) {
-    this.not_exist_col_error(col);
-    this.differ_type_error(col, value);
-    this[col] = value;
-    return this;
-  }
-
-  get_class_name() {
-    return this.constructor.name;
-  }
-
-  columns() {
-    return Object.getOwnPropertyNames(this).filter((col) => col !== "id");
-  }
-
-  types() {
-    return this.columns().map((col) => type_into_sql[typeof this[col]]);
-  }
-
-  values() {
-    return Object.values(this).slice(1);
-  }
-
-  to_sql() {
-    const types = this.types();
-    return this.columns().map((value, index) => {
-      return { col: value, type: types[index] };
-    });
-  }
-
-  to_entity(object) {
-    const entity = new this.constructor();
-    Object.keys(this).map((key) => {
-      entity[key] = object[key];
-    });
-    return entity;
-  }
-
-  /**
-   * 에러 관련 함수
-   */
-  not_exist_col_error(col) {
-    if (!this.columns().includes(col)) {
-      throw new Error("존재하지 않는 칼럼입니다.");
-    }
-  }
-
-  differ_type_error(col, value) {
-    if (typeof this[col] !== typeof value) {
-      throw new Error("주입한 값의 타입이 다릅니다.");
-    }
-  }
 }
 
 const type_into_sql = {
